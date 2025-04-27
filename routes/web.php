b8d084e6af9ad23658eeaf26c446b6389ea2dc7c
Ammar-gif-svg <ammar@qltyss.com>
2025-04-27 09:13:45 +0300

    gix bug

--- routes/web.php
+++ routes/web.php
@@ -149,7 +149,7 @@ Route::middleware(['auth', 'role:admin', 'prevent-back-history', 'custom-session
     Route::get('/managers/create', [ManagerController::class, 'create'])->name('managers.create');
     Route::post('/managers', [ManagerController::class, 'store'])->name('managers.store');
     Route::get('/managers/{manager}/edit', [ManagerController::class, 'edit'])->name('managers.edit');
-    Route::patch('/managers/{manager}', [ManagerController::class, 'update'])->name('managers.update');
+    Route::put('/managers/{manager}', [ManagerController::class, 'update'])->name('managers.update');
     Route::delete('/managers/{manager}', [ManagerController::class, 'destroy'])->name('managers.destroy');
 });
 
